8d6bad4f9afaba2d9f1fb5d5b48b6bdc3f28be90
rodarribas <147284169+rodarribas@users.noreply.github.com>
2023-10-08 13:43:15 +0200

    Version 2

--- hlf_funciones.py
+++ hlf_funciones.py
@@ -77,143 +77,22 @@ def comprobar_casillas_v(tamaño:int, tablero:np.ndarray, fila:int, columna:int,
 def posicionar_barco(tamaño:int, tablero:np.ndarray):
     orientacion = random.randint(0,1)
     prov_list = []
-    '''if tamaño == 1:
-        fila = random.randint(0, 9)
-        columna = random.randint(0, 9)
-        #comprobamos si ha elegido una posición en la que ya hay un barco
-        if tablero[fila][columna] == "O":
-            posicionar_barco(tamaño, tablero)
-        else:
-            try:
-                #comprobamos la casilla por encima
-                prov_list.append(tablero[fila - 1][columna])
-            except: pass
-            try:
-                #comprobamos la casilla por debajo
-                prov_list.append(tablero[fila + 1][columna])
-            except: pass
-            try:
-                #comprobamos la casilla a la derecha
-                prov_list.append(tablero[fila][columna + 1])
-            except: pass
-            try:
-                #comprobamos la casilla a la izquierda
-                prov_list.append(tablero[fila][columna - 1])
-            except: pass
-            try:
-                #comprobamos la casilla arriba a la derecha
-                prov_list.append(tablero[fila - 1][columna + 1])
-            except: pass
-            try:
-                #comprobamos la casilla arriba a la izquierda
-                prov_list.append(tablero[fila - 1][columna - 1])
-            except: pass
-            try:
-                #comprobamos la casilla abajo a la derecha
-                prov_list.append(tablero[fila + 1][columna + 1])
-            except: pass
-            try:
-                #comprobamos la casilla abajo a la izquierda
-                prov_list.append(tablero[fila + 1][columna - 1])
-            except: pass
-            if 'O' in prov_list:
-                posicionar_barco(tamaño, tablero)
-            else:
-                tablero[fila][columna] = "O"
-            return tablero'''
     if orientacion == 0: #horizontal
         #generamos una posición aleatoria de la que partir
         fila = random.randint(0, 9)
         columna = random.randint(0, 10-tamaño)
         #comprobamos casillas de acuerdo al tamaño del barco y las vamos metiendo en la lista prov_list
         comprobar_casillas_h(tamaño, tablero, fila, columna, prov_list)
-        '''for i in range(0,tamaño):
-            #comprobamos las casillas donde irá el barco                            
-            prov_list.append(tablero[fila][columna+i])
-            try:
-                #comprobamos las casillas por encima
-                prov_list.append(tablero[fila - 1][columna+i])
-            except: pass
-            try:
-                #comprobamos las casillas por debajo
-                prov_list.append(tablero[fila + 1][columna+i])
-            except: pass
-            #comprobamos casos para la primera casilla
-            if i == 0:
-                try:
-                    #comprobamos la casilla a la izquierda
-                    prov_list.append(tablero[fila][columna - 1])
-                except: pass
-                try:
-                    #comprobamos la casilla arriba a la izquierda
-                    prov_list.append(tablero[fila - 1][columna - 1])
-                except: pass
-                try:
-                    #comprobamos la casilla abajo a la izquierda
-                    prov_list.append(tablero[fila + 1][columna - 1])
-                except: pass
-            #comprobamos casos para la última casilla
-            elif i == tamaño - 1:
-                try:
-                    #comprobamos la casilla a la derecha
-                    prov_list.append(tablero[fila][columna + 1 + i])
-                except: pass
-                try:
-                    #comprobamos la casilla arriba a la derecha
-                    prov_list.append(tablero[fila - 1][columna + 1 + i])
-                except: pass
-                try:
-                    #comprobamos la casilla abajo a la derecha
-                    prov_list.append(tablero[fila + 1][columna + 1 + i])
-                except: pass'''
         if 'O' in prov_list:
             posicionar_barco(tamaño, tablero)
         else:
             for i in range(0,tamaño):
-                tablero[fila][columna+i] = "O"
+                tablero[fila][columna + i] = "O"
     elif orientacion == 1: #vertical
         fila = random.randint(0, 10-tamaño)
         columna = random.randint(0, 9)
         comprobar_casillas_v(tamaño, tablero, fila, columna, prov_list)
         #comprobamos casillas de acuerdo al tamaño del barco y las vamos metiendo en la lista prov_list
-        '''for i in range(0,tamaño):
-            prov_list.append(tablero[fila+i][columna])
-            try:
-                #comprobamos las casillas por la derecha
-                prov_list.append(tablero[fila+i][columna+1])
-            except: pass
-            try:
-                #comprobamos las casillas por la izquierda
-                prov_list.append(tablero[fila+i][columna-1])
-            except: pass
-            #comprobamos casos para la primera casilla
-            if i == 0:
-                try:
-                    #comprobamos la casilla arriba
-                    prov_list.append(tablero[fila-1][columna])
-                except: pass
-                try:
-                    #comprobamos la casilla arriba a la izquierda
-                    prov_list.append(tablero[fila-1][columna - 1])
-                except: pass
-                try:
-                    #comprobamos la casilla arriba a la derecha
-                    prov_list.append(tablero[fila-1][columna + 1])
-                except: pass
-            #comprobamos casos para la última casilla
-            elif i == tamaño - 1:
-                try:
-                    #comprobamos la casilla por debajo
-                    prov_list.append(tablero[fila + i + 1][columna])
-                except: pass
-                try:
-                    #comprobamos la casilla abajo a la izquierda
-                    prov_list.append(tablero[fila + i + 1][columna - 1])
-                except: pass
-                try:
-                    #comprobamos la casilla abajo a la derecha
-                    prov_list.append(tablero[fila + i + 1][columna + 1])
-                except: pass'''
         if 'O' in prov_list:
             posicionar_barco(tamaño, tablero)
         else:
@@ -225,6 +104,7 @@ def crear_tablero_full():
     tablero = np.full((10,10), " ")
     posicionar_barco(2, tablero)
     posicionar_barco(2, tablero)
+    posicionar_barco(3, tablero)
     posicionar_barco(3, tablero)
     posicionar_barco(4, tablero)
     posicionar_barco(1, tablero)
@@ -238,38 +118,100 @@ def crear_tablero_demo():
     posicionar_barco(4, tablero)
     return tablero
 
-def disparo_aleatorio(tablero_jugador:np.ndarray):
+def disparo_aleatorio(tablero_jugador:np.ndarray, tablero_maquina:np.ndarray, jugador:str):
+    print(f"Es el turno de la máquina\nTABLERO {jugador.upper()}")
+    print(tablero_jugador)
+    print("TABLERO MAQUINA")
+    print(tablero_maquina)
     a = random.randint(0, 9)
     b = random.randint(0, 9)
     target = tablero_jugador[a][b]
     if target == ' ':
         tablero_jugador[a][b] = '-'
-        print("Turno de la máquina: ¡agua!")
-        return False
+        print("Máquina: ¡agua!")
     elif target == 'O':
         tablero_jugador[a][b] = 'X'
-        print("Turno de la máquina: ¡tocado!")
-        return True
+        print("Máquina: ¡tocado!")
+        if 'O' not in tablero_jugador:
+            return None
+        return disparo_aleatorio(tablero_jugador, tablero_maquina, jugador)
     elif target == '-' or target == 'X':
-        print('La maquina ha elegido un objetivo no valido, va a volver disparar')
-        return disparo_aleatorio(tablero_jugador)
+        print('La maquina ha elegido un objetivo no válido, va a volver disparar')
+        return disparo_aleatorio(tablero_jugador, tablero_maquina, jugador)
 
-def disparo_jugador(tablero_maquina:np.ndarray):
+def disparo_jugador(tablero_maquina:np.ndarray, tablero_jugador:np.ndarray, jugador:str):
+    print(f"TABLERO {jugador.upper()}")
+    print(tablero_jugador)
+    print("TABLERO MAQUINA")
+    print(tablero_maquina)
+    print(f"Te toca {jugador}")
     a = int(input("Elige una fila para tu disparo: "))
     b = int(input("Elige una columna para tu disparo: "))
     try:
         target = tablero_maquina[a][b]
     except:
         print('Has disparado fuera del tablero, repite el tiro')
-        return disparo_jugador(tablero_maquina)
+        return disparo_jugador(tablero_maquina, tablero_jugador, jugador)
     if target == ' ':
         tablero_maquina[a][b] = '-'
         print("¡Agua!")
-        return False
     elif target == 'O':
         tablero_maquina[a][b] = 'X'
         print("¡Tocado!")
-        return True
+        if 'O' not in tablero_maquina:
+            return None
+        return disparo_jugador(tablero_maquina, tablero_jugador, jugador)
     elif target == '-' or target == 'X':
         print('Has escogido una casilla donde ya habías disparado, prueba otra vez')
-        return disparo_jugador(tablero_maquina)
+        return disparo_jugador(tablero_maquina, tablero_jugador, jugador)
+    
+def disparo_aleatorio_f(tablero_jugador:np.ndarray, tablero_maquina:np.ndarray, jugador:str):
+    print(f"Es el turno de la máquina\nTABLERO {jugador.upper()}")
+    print(tablero_jugador)
+    print("TABLERO MAQUINA")
+    tablero_maquina_visible = tablero_maquina.copy()
+    tablero_maquina_visible[tablero_maquina_visible == 'O'] = ' '
+    print(tablero_maquina_visible)
+    a = random.randint(0, 9)
+    b = random.randint(0, 9)
+    target = tablero_jugador[a][b]
+    if target == ' ':
+        tablero_jugador[a][b] = '-'
+        print("Máquina: ¡agua!")
+    elif target == 'O':
+        tablero_jugador[a][b] = 'X'
+        print("Máquina: ¡tocado!")
+        if 'O' not in tablero_jugador:
+            return None
+        return disparo_aleatorio_f(tablero_jugador, tablero_maquina, jugador)
+    elif target == '-' or target == 'X':
+        print('La maquina ha elegido un objetivo no válido, va a volver disparar')
+        return disparo_aleatorio_f(tablero_jugador, tablero_maquina, jugador)
+    
+def disparo_jugador_f(tablero_maquina:np.ndarray, tablero_jugador:np.ndarray, jugador:str):
+    print(f"TABLERO {jugador.upper()}")
+    print(tablero_jugador)
+    print("TABLERO MAQUINA")
+    tablero_maquina_visible = tablero_maquina.copy()
+    tablero_maquina_visible[tablero_maquina_visible == 'O'] = ' '
+    print(tablero_maquina_visible)
+    print(f"Te toca {jugador}")
+    a = int(input("Elige una fila para tu disparo: "))
+    b = int(input("Elige una columna para tu disparo: "))
+    try:
+        target = tablero_maquina[a][b]
+    except:
+        print('Has disparado fuera del tablero, repite el tiro')
+        return disparo_jugador_f(tablero_maquina, tablero_jugador, jugador)
+    if target == ' ':
+        tablero_maquina[a][b] = '-'
+        print("¡Agua!")
+    elif target == 'O':
+        tablero_maquina[a][b] = 'X'
+        print("¡Tocado!")
+        if 'O' not in tablero_maquina:
+            return None
+        return disparo_jugador_f(tablero_maquina, tablero_jugador, jugador)
+    elif target == '-' or target == 'X':
+        print('Has escogido una casilla donde ya habías disparado, prueba otra vez')
+        return disparo_jugador_f(tablero_maquina, tablero_jugador, jugador)
--- hlf_main.py
+++ hlf_main.py
@@ -2,11 +2,11 @@ from hlf_funciones import *
 
 print('Bienvenido a hundir la flota, una aventura llena de emoción y gráficos hiperrealistas')
 
-jugador = input('Introduce tu nombre: ')
+jugador:str = input('Introduce tu nombre: ')
 
-game_mode = input('Elige el modo de juego: \n- Para jugar a la demo pulsa 1 \n- Para jugar a la version completa pulsa 2\n')
+game_mode = input('Elige el modo de juego:\n- Para jugar a la demo pulsa 1\n- Para jugar a la version completa pulsa 2\n')
 while game_mode not in ['1','2']:
-    game_mode = input('El número introducido no es válido, vuelve a intentarlo \n')
+    game_mode = input('El número introducido no es válido, vuelve a intentarlo\n')
 
 print('\n¡Comienza el juego!\n')
 
@@ -16,69 +16,27 @@ if game_mode == '1':
     tablero_jugador = crear_tablero_demo()
 
     while ("O" in tablero_jugador) or ("O" in tablero_maquina):
-        while True:
-            print("TABLERO JUGADOR")
-            print(tablero_jugador)
-            print("TABLERO MAQUINA")
-            print(tablero_maquina)
-            print(f"Te toca {jugador}")
-            tocado = disparo_jugador(tablero_maquina)
-            if tocado == False or "O" not in tablero_maquina:
-                break
+        disparo_jugador(tablero_maquina, tablero_jugador, jugador)
         if "O" not in tablero_maquina:
             break
-        while True:
-            print("Es el turno de la máquina \nTABLERO JUGADOR")
-            print(tablero_jugador)
-            print("TABLERO MAQUINA")
-            print(tablero_maquina)
-            tocado = disparo_aleatorio(tablero_jugador)
-            if tocado == False or 'O' not in tablero_jugador:
-                break
-
-    print("TABLERO JUGADOR")
-    print(tablero_jugador)
-    print("TABLERO MAQUINA")
-    print(tablero_maquina)
-    if "O" not in tablero_jugador: 
-        print("¡Has perdido!")
-    elif "O" not in tablero_maquina:
-        print("¡Has ganado!")
+        disparo_aleatorio(tablero_jugador, tablero_maquina, jugador)
 
 elif game_mode == '2':
+    
     tablero_maquina = crear_tablero_full()
     tablero_jugador = crear_tablero_full()
 
     while ("O" in tablero_jugador) or ("O" in tablero_maquina):
-        while True:
-            print("TABLERO JUGADOR")
-            print(tablero_jugador)
-            print("TABLERO MAQUINA")
-            tablero_maquina_visible = tablero_maquina.copy()
-            tablero_maquina_visible[tablero_maquina_visible == 'O'] = ' '
-            print(tablero_maquina_visible)
-            print(f"Te toca {jugador}")
-            tocado = disparo_jugador(tablero_maquina)
-            if tocado == False or "O" not in tablero_maquina:
-                break
+        disparo_jugador_f(tablero_maquina, tablero_jugador, jugador)
         if "O" not in tablero_maquina:
             break
-        while True:
-            print("Es el turno de la máquina \nTABLERO JUGADOR")
-            print(tablero_jugador)
-            print("TABLERO MAQUINA")
-            tablero_maquina_visible = tablero_maquina.copy()
-            tablero_maquina_visible[tablero_maquina_visible == 'O'] = ' '
-            print(tablero_maquina_visible)
-            tocado = disparo_aleatorio(tablero_jugador)
-            if tocado == False or 'O' not in tablero_jugador:
-                break
+        disparo_aleatorio_f(tablero_jugador, tablero_maquina, jugador)
 
-    print("TABLERO JUGADOR")
-    print(tablero_jugador)
-    print("TABLERO MAQUINA")
-    print(tablero_maquina)
-    if "O" not in tablero_jugador: 
-        print("¡Has perdido!")
-    elif "O" not in tablero_maquina:
-        print("¡Has ganado!")
+print(f"TABLERO {jugador.upper()}")
+print(tablero_jugador)
+print("TABLERO MAQUINA")
+print(tablero_maquina)
+if "O" not in tablero_jugador: 
+    print(f"Mala suerte {jugador}, ¡has perdido!")
+elif "O" not in tablero_maquina:
+    print(f"Enhorabuena {jugador}, ¡has ganado!")
